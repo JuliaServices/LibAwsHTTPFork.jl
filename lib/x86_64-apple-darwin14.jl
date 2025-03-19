2a3624a299d760333bbace555192f409609df6ce
quinnj2 <karbarcca2@gmail.com>
2025-03-19 08:03:16 -0600

    Merge pull request #1 from JuliaServices/update-aws_c_http_jq_jll-0.9.5
    Update aws_c_http_jq_jll to version 0.9.5

--- lib/x86_64-apple-darwin14.jl
+++ lib/x86_64-apple-darwin14.jl
@@ -635,6 +635,7 @@ struct aws_http_connection_manager_options
     bootstrap::Ptr{aws_client_bootstrap}
     initial_window_size::Csize_t
     socket_options::Ptr{aws_socket_options}
+    response_first_byte_timeout_ms::UInt64
     tls_connection_options::Ptr{aws_tls_connection_options}
     http2_prior_knowledge::Bool
     monitoring_options::Ptr{aws_http_connection_monitoring_options}
@@ -651,6 +652,8 @@ struct aws_http_connection_manager_options
     shutdown_complete_callback::Ptr{aws_http_connection_manager_shutdown_complete_fn}
     enable_read_back_pressure::Bool
     max_connection_idle_in_milliseconds::UInt64
+    connection_acquisition_timeout_ms::UInt64
+    max_pending_connection_acquisitions::UInt64
     network_interface_names_array::Ptr{aws_byte_cursor}
     num_network_interface_names::Csize_t
 end
@@ -785,6 +788,8 @@ Documentation not found.
     AWS_ERROR_HTTP_MANUAL_WRITE_NOT_ENABLED = 2090
     AWS_ERROR_HTTP_MANUAL_WRITE_HAS_COMPLETED = 2091
     AWS_ERROR_HTTP_RESPONSE_FIRST_BYTE_TIMEOUT = 2092
+    AWS_ERROR_HTTP_CONNECTION_MANAGER_ACQUISITION_TIMEOUT = 2093
+    AWS_ERROR_HTTP_CONNECTION_MANAGER_MAX_PENDING_ACQUISITIONS_EXCEEDED = 2094
     AWS_ERROR_HTTP_END_RANGE = 3071
 end
 
@@ -1193,28 +1198,28 @@ struct aws_http_proxy_negotiator_tunnelling_vtable
 end
 
 """
-    __JL_Ctag_137
+    union (unnamed at /home/runner/.julia/artifacts/2d09d8dcf7dab79f2d344de3121b63c844bf8c2e/include/aws/http/proxy.h:302:5)
 
 Documentation not found.
 """
-struct __JL_Ctag_137
+struct var"union (unnamed at /home/runner/.julia/artifacts/2d09d8dcf7dab79f2d344de3121b63c844bf8c2e/include/aws/http/proxy.h:302:5)"
     data::NTuple{8, UInt8}
 end
 
-function Base.getproperty(x::Ptr{__JL_Ctag_137}, f::Symbol)
+function Base.getproperty(x::Ptr{var"union (unnamed at /home/runner/.julia/artifacts/2d09d8dcf7dab79f2d344de3121b63c844bf8c2e/include/aws/http/proxy.h:302:5)"}, f::Symbol)
     f === :forwarding_vtable && return Ptr{Ptr{aws_http_proxy_negotiator_forwarding_vtable}}(x + 0)
     f === :tunnelling_vtable && return Ptr{Ptr{aws_http_proxy_negotiator_tunnelling_vtable}}(x + 0)
     return getfield(x, f)
 end
 
-function Base.getproperty(x::__JL_Ctag_137, f::Symbol)
-    r = Ref{__JL_Ctag_137}(x)
-    ptr = Base.unsafe_convert(Ptr{__JL_Ctag_137}, r)
+function Base.getproperty(x::var"union (unnamed at /home/runner/.julia/artifacts/2d09d8dcf7dab79f2d344de3121b63c844bf8c2e/include/aws/http/proxy.h:302:5)", f::Symbol)
+    r = Ref{var"union (unnamed at /home/runner/.julia/artifacts/2d09d8dcf7dab79f2d344de3121b63c844bf8c2e/include/aws/http/proxy.h:302:5)"}(x)
+    ptr = Base.unsafe_convert(Ptr{var"union (unnamed at /home/runner/.julia/artifacts/2d09d8dcf7dab79f2d344de3121b63c844bf8c2e/include/aws/http/proxy.h:302:5)"}, r)
     fptr = getproperty(ptr, f)
     GC.@preserve r unsafe_load(fptr)
 end
 
-function Base.setproperty!(x::Ptr{__JL_Ctag_137}, f::Symbol, v)
+function Base.setproperty!(x::Ptr{var"union (unnamed at /home/runner/.julia/artifacts/2d09d8dcf7dab79f2d344de3121b63c844bf8c2e/include/aws/http/proxy.h:302:5)"}, f::Symbol, v)
     unsafe_store!(getproperty(x, f), v)
 end
 
@@ -1230,7 +1235,7 @@ end
 function Base.getproperty(x::Ptr{aws_http_proxy_negotiator}, f::Symbol)
     f === :ref_count && return Ptr{aws_ref_count}(x + 0)
     f === :impl && return Ptr{Ptr{Cvoid}}(x + 24)
-    f === :strategy_vtable && return Ptr{__JL_Ctag_137}(x + 32)
+    f === :strategy_vtable && return Ptr{var"union (unnamed at /home/runner/.julia/artifacts/2d09d8dcf7dab79f2d344de3121b63c844bf8c2e/include/aws/http/proxy.h:302:5)"}(x + 32)
     return getfield(x, f)
 end
 
@@ -1698,7 +1703,6 @@ Invoked when the data stream of an outgoing HTTP write operation is no longer in
 """
 const aws_http_stream_write_complete_fn = Cvoid
 
-# typedef aws_http_stream_write_complete_fn aws_http1_stream_write_chunk_complete_fn
 """
 Invoked when the data of an outgoing HTTP/1.1 chunk is no longer in use. This is always invoked on the HTTP connection's event-loop thread.
 
@@ -1733,7 +1737,6 @@ struct aws_http1_chunk_options
     user_data::Ptr{Cvoid}
 end
 
-# typedef aws_http_stream_write_complete_fn aws_http2_stream_write_data_complete_fn
 """
 Invoked when the data of an outgoing HTTP2 data frame is no longer in use. This is always invoked on the HTTP connection's event-loop thread.
 
@@ -2622,7 +2625,7 @@ end
 """
     aws_http1_stream_write_chunk(http1_stream, options)
 
-Submit a chunk of data to be sent on an HTTP/1.1 stream. The stream must have specified "chunked" in a "transfer-encoding" header. For client streams, activate() must be called before any chunks are submitted. For server streams, the response must be submitted before any chunks. A final chunk with size 0 must be submitted to successfully complete the HTTP-stream.
+Submit a chunk of data to be sent on an HTTP/1.1 stream. The stream must have specified "chunked" in a "transfer-encoding" header, and the [`aws_http_message`](@ref) must NOT have any body stream set. For client streams, activate() must be called before any chunks are submitted. For server streams, the response must be submitted before any chunks. A final chunk with size 0 must be submitted to successfully complete the HTTP-stream.
 
 Returns AWS\\_OP\\_SUCCESS if the chunk has been submitted. The chunk's completion callback will be invoked when the HTTP-stream is done with the chunk data, whether or not it was successfully sent (see [`aws_http1_stream_write_chunk_complete_fn`](@ref)). The chunk data must remain valid until the completion callback is invoked.
 
@@ -3445,6 +3448,20 @@ struct aws_websocket_client_connection_options
     host_resolution_config::Ptr{aws_host_resolution_config}
 end
 
+"""
+    aws_websocket_server_upgrade_options
+
+Documentation not found.
+"""
+struct aws_websocket_server_upgrade_options
+    initial_window_size::Csize_t
+    user_data::Ptr{Cvoid}
+    on_incoming_frame_begin::Ptr{aws_websocket_on_incoming_frame_begin_fn}
+    on_incoming_frame_payload::Ptr{aws_websocket_on_incoming_frame_payload_fn}
+    on_incoming_frame_complete::Ptr{aws_websocket_on_incoming_frame_complete_fn}
+    manual_window_management::Bool
+end
+
 # typedef bool ( aws_websocket_stream_outgoing_payload_fn ) ( struct aws_websocket * websocket , struct aws_byte_buf * out_buf , void * user_data )
 """
 Called repeatedly as the websocket's payload is streamed out. The user should write payload data to out\\_buf, up to available capacity. The websocket will mask this data for you, if necessary. Invoked repeatedly on the websocket's event-loop thread.
@@ -3643,6 +3660,50 @@ struct aws_http_message *aws_http_message_new_websocket_handshake_request( struc
 """
 function aws_http_message_new_websocket_handshake_request(allocator, path, host)
     ccall((:aws_http_message_new_websocket_handshake_request, libaws_c_http_jq), Ptr{aws_http_message}, (Ptr{aws_allocator}, aws_byte_cursor, aws_byte_cursor), allocator, path, host)
+end
+
+"""
+    aws_websocket_is_websocket_request(request)
+
+Return true if the request is a valid websocket upgrade request.
+
+### Prototype
+```c
+bool aws_websocket_is_websocket_request(const struct aws_http_message *request);
+```
+"""
+function aws_websocket_is_websocket_request(request)
+    ccall((:aws_websocket_is_websocket_request, libaws_c_http_jq), Bool, (Ptr{aws_http_message},), request)
+end
+
+"""
+    aws_http_message_new_websocket_handshake_response(allocator, accept_key)
+
+Create response with all required fields for a websocket upgrade response. The following headers are added:
+
+Upgrade: websocket Connection: Upgrade Sec-WebSocket-Accept: <base64 encoded accept key>
+
+### Prototype
+```c
+struct aws_http_message *aws_http_message_new_websocket_handshake_response( struct aws_allocator *allocator, struct aws_byte_cursor accept_key);
+```
+"""
+function aws_http_message_new_websocket_handshake_response(allocator, accept_key)
+    ccall((:aws_http_message_new_websocket_handshake_response, libaws_c_http_jq), Ptr{aws_http_message}, (Ptr{aws_allocator}, aws_byte_cursor), allocator, accept_key)
+end
+
+"""
+    aws_websocket_upgrade(allocator, stream, options)
+
+Upgrade an incoming HTTP connection to a websocket connection. This function should be called from the on\\_request\\_done callback of a request handler. It expects a fully constructed request and will handle sending the handshake response and install the websocket handler into the channel.
+
+### Prototype
+```c
+struct aws_websocket *aws_websocket_upgrade( struct aws_allocator *allocator, struct aws_http_stream *stream, const struct aws_websocket_server_upgrade_options *options);
+```
+"""
+function aws_websocket_upgrade(allocator, stream, options)
+    ccall((:aws_websocket_upgrade, libaws_c_http_jq), Ptr{aws_websocket}, (Ptr{aws_allocator}, Ptr{aws_http_stream}, Ptr{aws_websocket_server_upgrade_options}), allocator, stream, options)
 end
 
 """
